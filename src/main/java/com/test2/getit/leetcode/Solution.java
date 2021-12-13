84e870ba867ac7510c1e15b16d7b08968cf9a24f
Gavin218 <893025984@qq.com>
2021-12-14 01:43:10 +0800

    加入了test类的注解

--- src/main/java/com/test2/getit/leetcode/Solution.java
+++ src/main/java/com/test2/getit/leetcode/Solution.java
@@ -1,5 +1,11 @@
 package com.test2.getit.leetcode;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
 /**
  * @ClassName: Solution
  * @Description:
@@ -8,7 +14,7 @@ package com.test2.getit.leetcode;
  * @Version 1.0
  */
 
-
+@Component
 public class Solution {
 
     public int[][] colorBorder(int[][] grid, int row, int col, int color) {
@@ -55,6 +61,95 @@ public class Solution {
             }
         }
         return false;
+    }
+
+    public String toLowerCase(String s) {
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            int theASCII = s.charAt(i);
+            if (theASCII >= 65 && theASCII <= 90) {
+                sb.append((char)(theASCII + 32));
+            }
+            else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int[][] sight = getSight(grid);
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                result += Math.min(sight[0][i], sight[1][j]) - grid[i][j];
+            }
+        }
+        return result;
+    }
+    public int[][] getSight(int[][] grid) {
+        int[][] result = new int[2][grid.length];
+        int[][] tGrid = new int[grid[0].length][grid.length];
+        for (int i = 0; i < grid.length; i++) {
+            int maxNum = -Integer.MIN_VALUE;
+            for (int j = 0; j < grid[0].length; j++) {
+                maxNum = (grid[i][j] > maxNum) ? grid[i][j] : maxNum;
+                tGrid[j][i] = grid[i][j];
+            }
+            result[0][i] = maxNum;
+        }
+        for (int i = 0; i < tGrid.length; i++) {
+            int maxNum = -Integer.MIN_VALUE;
+            for (int j = 0; j < tGrid[0].length; j++) {
+                maxNum = (tGrid[i][j] > maxNum) ? tGrid[i][j] : maxNum;
+            }
+            result[1][i] = maxNum;
+        }
+        return result;
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (null == root) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (null != node.left || null != node.right) {
+                TreeNode tem = node.left;
+                node.left = node.right;
+                node.right = tem;
+            }
+            if (null != node.left) {
+                queue.add(node.left);
+            }
+            if (null != node.right) {
+                queue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        String[] fizzBuzz = {"FizzBuzz", "Fizz", "Buzz"};
+        for (int i = 1; i <= n; i++) {
+            if ((i%3)==0 && (i%5)==0) {
+                list.add(fizzBuzz[0]);
+            }
+            else if ((i%3)==0) {
+                list.add(fizzBuzz[1]);
+            }
+            else if ((i%5)==0) {
+                list.add(fizzBuzz[2]);
+            }
+            else{
+                list.add(Integer.toString(i));
+            }
+        }
+        return list;
     }
 
 }
--- src/test/java/com/test2/getit/LeetCode/SolutionTest.java
+++ src/test/java/com/test2/getit/LeetCode/SolutionTest.java
@@ -1,13 +1,27 @@
 package com.test2.getit.LeetCode;
 
+import com.fasterxml.jackson.datatype.jdk8.OptionalIntDeserializer;
+import com.test2.getit.GetitApplication;
 import com.test2.getit.leetcode.Solution;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.OptionalInt;
 
 import static org.junit.jupiter.api.Assertions.*;
+@TestConfiguration
+
+@SpringBootTest(classes = GetitApplication.class)
 
 class SolutionTest {
+
+    @Autowired
+    private Solution solution;
 
     @Test
     void canConstruct() {
@@ -18,7 +32,31 @@ class SolutionTest {
     @Test
     void colorBorder() {
         int[][] x = {{1,1},{1,2}};
-        int[][] y = new Solution().colorBorder(x, 0, 0, 3);
+        int[][] y = solution.colorBorder(x, 0, 0, 3);
         System.out.println(y);
+    }
+
+    @Test
+    void toLowerCase() {
+        System.out.println(solution.toLowerCase("Hello"));
+    }
+
+    @Test
+    void maxIncreaseKeepingSkyline() {
+        int[][] x = {{3, 0, 8, 4},{2, 4, 5, 7},{9, 2, 6, 3},{0, 3, 1, 0}};
+        int y = solution.maxIncreaseKeepingSkyline(x);
+        System.out.println(y);
+        System.out.println(Arrays.stream(x[0]).min());
+        int s = 5;
+        String ss = Integer.toString(s);
+        System.out.println(ss);
+
+
+    }
+
+    @Test
+    void fizzBuzz() {
+        List<String> list = solution.fizzBuzz(15);
+        System.out.println(list);
     }
 }
